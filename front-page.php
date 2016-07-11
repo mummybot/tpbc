80c186cae31e2be63e3eaf762f565fdff1537039
Francis Saul <francis@mummybot.com>
2016-07-11 15:31:30 +0200

    Fixed Navigation bug

--- front-page.php
+++ front-page.php
@@ -37,7 +37,7 @@ get_header(); ?>
 
         <section class="section sectionHomeContent">
         <?php
-        // Get Our sponsors page
+        // Get "Our sponsors" page
         global $post;
         $post = get_post( 2 );
         setup_postdata($post);
@@ -48,26 +48,6 @@ get_header(); ?>
              */
             get_template_part( 'template-parts/content', get_post_format() );
         ?>
-        </section>
-
-        <section class="inverse section sectionHomeBottom">
-            <div class="colWrapper colVariant01">
-                <div class="col3">
-                    <?php if ( is_active_sidebar( 'home_bottom_widgets_1' ) ) : ?>
-                        <?php dynamic_sidebar( 'home_bottom_widgets_1' ); ?>
-                    <?php endif; ?>
-                </div>
-                <div class="col3">
-                    <?php if ( is_active_sidebar( 'home_bottom_widgets_2' ) ) : ?>
-                        <?php dynamic_sidebar( 'home_bottom_widgets_2' ); ?>
-                    <?php endif; ?>
-                </div>
-                <div class="col3">
-                    <?php if ( is_active_sidebar( 'home_bottom_widgets_3' ) ) : ?>
-                        <?php dynamic_sidebar( 'home_bottom_widgets_3' ); ?>
-                    <?php endif; ?>
-                </div>
-            </div>
         </section>
 
 <?php get_footer(); ?>
--- src/components/footer/footer.php
+++ src/components/footer/footer.php
@@ -1,3 +1,23 @@
+        <section class="inverse section sectionHomeBottom">
+            <div class="colWrapper colVariant01">
+                <div class="col3">
+                    <?php if ( is_active_sidebar( 'home_bottom_widgets_1' ) ) : ?>
+                        <?php dynamic_sidebar( 'home_bottom_widgets_1' ); ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col3">
+                    <?php if ( is_active_sidebar( 'home_bottom_widgets_2' ) ) : ?>
+                        <?php dynamic_sidebar( 'home_bottom_widgets_2' ); ?>
+                    <?php endif; ?>
+                </div>
+                <div class="col3">
+                    <?php if ( is_active_sidebar( 'home_bottom_widgets_3' ) ) : ?>
+                        <?php dynamic_sidebar( 'home_bottom_widgets_3' ); ?>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </section>
+
         <footer id="colophon" class="site-footer inverse" role="contentinfo">
             <?php if ( has_nav_menu( 'social' ) ) : ?>
                 <nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'tpbc' ); ?>">
