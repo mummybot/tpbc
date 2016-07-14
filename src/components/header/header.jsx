2352aaefab41bc86fe01fdea5ee035460a91c148
Francis Saul <francis@mummybot.com>
2016-07-14 12:50:36 +0200

    Cross browser mobile menu improvements

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,6 @@
 import Hamburger from './components/hamburger/hamburger';
-import Search from './components/search/search';
 import React from 'react';
+import Search from './components/search/search';
 import _logo from './components/logo/logo';
 import _navigationMain from './components/navigationMain/navigationMain';
 import {hashHistory} from 'react-router';
@@ -12,7 +12,7 @@ const CLOSED = 'closed';
 const TRANSITION_CLOSED = 'transitionClosed';
 const TRANSITION_OPEN = 'transitionOpen';
 const AUTO = 'auto';
-const BREAKPOINT_TABLET = 400;
+const BREAKPOINT_TABLET = 800;
 
 export default React.createClass({
   propTypes: {
@@ -22,7 +22,8 @@ export default React.createClass({
   },
   getInitialState() {
     return {
-      menu: this.props.params.menu ? OPEN : CLOSED
+      menu: this.props.params.menu ? OPEN : CLOSED,
+      isMobile: false
     };
   },
   componentDidMount() {
@@ -32,6 +33,7 @@ export default React.createClass({
 
     window.addEventListener('hashchange', this.handleHashChange);
     window.addEventListener('resize', this.updateBrowser);
+    window.addEventListener('orientationchange', this.updateBrowser);
 
     this.updateBrowser();
   },
@@ -40,6 +42,7 @@ export default React.createClass({
 
     window.removeEventListener('hashchange', this.handleHashChange);
     window.removeEventListener('resize', this.updateBrowser);
+    window.removeEventListener('orientationchange', this.updateBrowser);
   },
   setStyles(width, height, left) {
     const style = this.domNode.style;
@@ -47,32 +50,41 @@ export default React.createClass({
     style.width = width;
     style.height = height;
     style.left = left;
+
+    this.bodyNode.style.paddingTop = `${this.headerNode.offsetHeight}px`;
+  },
+  isMobile(callback) {
+    if (window.innerWidth >= BREAKPOINT_TABLET) {
+      this.setState({isMobile: false}, callback);
+    } else {
+      this.setState({isMobile: true}, callback);
+    }
   },
   updateBrowser() {
-    this.bodyNode.style.paddingTop = `${this.headerNode.offsetHeight}px`;
-
-    // Menu is closed
-    if (this.state.menu === CLOSED) {
-      if (window.innerWidth >= BREAKPOINT_TABLET) {
+    this.isMobile(() => {
+      // Menu is closed
+      if (this.state.menu === CLOSED) {
+        if (this.state.isMobile === false) {
+          this.setStyles(AUTO, AUTO, AUTO);
+        } else {
+          this.setStyles(
+            `${window.innerWidth}px`,
+            `${window.innerHeight}px`,
+            `${-1 * window.innerWidth}px`
+          );
+        }
+      } else
+      // Menu is open
+      if (this.state.isMobile === false) {
         this.setStyles(AUTO, AUTO, AUTO);
       } else {
         this.setStyles(
           `${window.innerWidth}px`,
           `${window.innerHeight}px`,
-          `${-1 * window.innerWidth}px`
+          0
         );
       }
-    } else
-    // Menu is open
-    if (window.innerWidth >= BREAKPOINT_TABLET) {
-      this.setStyles(AUTO, AUTO, AUTO);
-    } else {
-      this.setStyles(
-        `${window.innerWidth}px`,
-        `${window.innerHeight}px`,
-        0
-      );
-    }
+    });
   },
   handleHashChange() {
     if (window.location.hash && window.location.hash.indexOf('menu') !== -1) {
