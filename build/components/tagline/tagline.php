9838334946235fc02a1f0d2710dea71d2d6c8428
Francis Saul <francis@mummybot.com>
2016-07-17 10:14:13 +0200

    Added React to build for HTMLtoJSX

--- build/components/tagline/tagline.php
+++
@@ -1,5 +0,0 @@
-<?php
-$description = get_bloginfo( 'description', 'display' );
-if ( $description || is_customize_preview() ) : ?>
-	<p class="tagline"><?php echo html_entity_decode($description); ?></p>
-<?php endif; ?>
